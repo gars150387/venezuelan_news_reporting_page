200ad4bcafa0f2147bcc0f8f6df1c6338095d223
Gustavo <grodriguez@contextglobal.com>
2024-11-29 07:07:32 -0500

    animation for banner

--- src/components/BannerVenezuelanAdjustmentLaw.jsx
+++ src/components/BannerVenezuelanAdjustmentLaw.jsx
@@ -1,20 +1,59 @@
+import { useScroll, useTransform, motion } from "framer-motion";
+import { useRef } from "react";
+
 export default function BannerVenezuelanAdjustmentLaw() {
+  const textref = useRef(null);
+  const aRef = useRef(null);
+  const { scrollY } = useScroll();
+
+  const scale = useTransform(scrollY, [0, 120], [1, 0]);
+  const height = useTransform(scrollY, [0, 240], ["fit-content", 0]);
+  const display = useTransform(scrollY, [0, 240], ["flex", "none"]);
+  const fontSize = useTransform(scrollY, [0, 240], ["1.875rem", "1rem"]);
+  const lineHeight = useTransform(scrollY, [0, 240], ["2.25rem", "1.25rem"]);
   return (
-    <div className="bg-gradient-to-b from-white to-transparent text-blue-800 shadow-md py-4">
-      <div className="container mx-auto pb-4 px-4 my-4">
-        <p className="text-center mt-4 text-blue-800 text-xl font-bold drop-shadow-lg">
+    <div className="w-full bg-gradient-to-b from-white to-transparent text-blue-800 shadow-md">
+      <div className="container mx-auto py-0 px-4 my-0 sm:py-0 sm:px-0 sm:my-0">
+        <motion.p
+          style={{
+            scale,
+            height,
+            display,
+          }}
+          transition={{
+            duration: 1,
+            delay: 0.5,
+          }}
+          initial={{
+            scale: 1,
+            height: "fit-content",
+            display: "flex",
+          }}
+          ref={textref}
+          className="text-center mt-4 text-blue-800 text-xl font-bold drop-shadow-lg"
+        >
           Esta ley beneficiaria a los venezolanos que se encuentran tanto en
           Venezuela como fuera de Venezuela. Todos pueden llenar el formulario
           sin necesidad de ser venezolano.
-        </p>
-        <a
+        </motion.p>
+        <motion.a
+          ref={aRef}
           href="https://leydeajustevenezolano.org/referidos-por-la-pagina-web/"
           target="_blank"
           rel="noreferrer"
-          className="text-purple-950 underline decoration-current text-3xl cursor-pointer decoration-blue-500 underline-offset-2 hover:text-blue-800"
+          className=" text-purple-950 underline decoration-current cursor-pointer underline-offset-2 text-balance px-0 hover:text-blue-800 my-4 sm:my-0"
+          style={{
+            fontSize,
+            lineHeight,
+            width: "100%",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        
         >
           Ley de ajuste venezolano.
-        </a>{" "}
+        </motion.a>{" "}
       </div>
     </div>
   );

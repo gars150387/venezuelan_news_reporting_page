23b76686a2a32ee74081c1c59deadde609103d35
Gustavo <grodriguez@contextglobal.com>
2024-12-04 12:28:58 -0500

    add extension for customized background image

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 export default {
     content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
     theme: {
-      extend: {},
+      extend: {
+        backgroundImage: {
+          'venezuela-flag': "url('https://leydeajustevenezolano.org/wp-content/uploads/2021/05/COVER-WEB.png')",
+          'lav-title': "url('https://leydeajustevenezolano.org/wp-content/uploads/2024/04/LOGO-LEY-DE-AJUSTE-7-STARS-07-1024x233.png')",
+        }
+      },
     },
     plugins: [],
   };
